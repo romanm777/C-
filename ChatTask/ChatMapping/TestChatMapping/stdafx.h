d667fd96a0e3aa241ae79e35d76b0319da7642d9
Mrychko <ext-roman.mrychko@here.com>
2017-04-14 16:53:03 +0300

    Test development started

---
+++ ChatTask/ChatMapping/TestChatMapping/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <time.h>
+#include <conio.h>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+// TODO: reference additional headers your program requires here
